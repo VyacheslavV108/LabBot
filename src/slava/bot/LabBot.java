1ff27e0a3a2b874c7bdc6e80a86d7b9cc89104ec
VyacheslavV108 <45122233+VyacheslavV108@users.noreply.github.com>
2020-04-14 21:47:21 +0300

    Add files via upload

---
+++ src/slava/bot/LabBot.java
@@ -0,0 +1,42 @@
+package slava.bot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import slava.bot.commands.Calculate;
+import slava.bot.commands.Clear;
+import slava.bot.commands.Filter;
+import slava.bot.commands.FilterMessage;
+import slava.bot.commands.FilterOnOff;
+import slava.bot.commands.HelloEvent;
+import slava.bot.events.GuildMemberJoin;
+import slava.bot.events.GuildMemberLeave;
+
+
+public class LabBot {
+	public static JDA jda;
+	
+	//ÇÀÄÀÅÌ ÁÎÒÓ ÏĞÅÔÈÊÑ ÄËß ÅÃÎ ÂÛÇÎÂÀ
+	public static String prefix = "~";
+	
+	//ÃËÀÂÍÛÉ ÌÅÒÎÄ
+	public static void main(String[] args ) throws LoginException{
+		jda =  new JDABuilder(AccountType.BOT).setToken("Njc4MjQ5NDY0MzQ2OTY4MDkw.XkgNnA.pHE0PWIroLmEpH16e88tRoGxp-M").build();
+		//ÄÅËÀÅÌ ÅÌÓ ÎÍËÀÉÍ ÑÒÀÒÓÑ
+		jda.getPresence().setStatus(OnlineStatus.ONLINE);
+		
+		jda.addEventListener(new Commands());
+		jda.addEventListener(new Clear());
+		jda.addEventListener(new GuildMemberJoin());
+		jda.addEventListener(new GuildMemberLeave());
+		jda.addEventListener(new HelloEvent());
+		jda.addEventListener(new Calculate());
+		jda.addEventListener(new Filter());
+		jda.addEventListener(new FilterMessage());
+		jda.addEventListener(new FilterOnOff());
+		
+	}
+}
